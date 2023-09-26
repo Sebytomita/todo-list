83b1c45778cac3db124363d71ed10fcb5ac3d9d8
Seby <Sebytomita05@gmail.com>
2023-09-26 17:09:02 +0300

    Added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'))
+    if (storedTodos.length > 0) {
+      setTodos(storedTodos)
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(text) {
+    setTodos([...todos, text])
+  }
+
+  function removeTodo(index) {
+    // Checks whether the index is equal
+    // Omits that item from the array
+    // Returns the rest of the items in the array
+    setTodos(todos.filter((todo, i) => i !== index))
+  }
+
+  function editTodo(index, text) {
+    const newTodos = [...todos]
+    newTodos[index] = text
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Todo List</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          addTodo(event.target.elements.todo.value)
+          event.target.elements.todo.value = ''
+        }}
+      >
+        <input type='text' name='todo' />
+        <button type='submit'>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="text"
+              value={todo}
+              onChange={(event) => editTodo(index, event.target.value)}
+            />
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+
     </div>
-  );
+  )
 }
 
 export default App;
